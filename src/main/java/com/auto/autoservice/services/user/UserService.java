6a6232bea204dbbd73ffd8dc775c2f7fbe1273b4
Niklas <mrnikolayzv@gmail.com>
2022-02-22 11:26:04 +0200

    User operations

---
+++ src/main/java/com/auto/autoservice/services/user/UserService.java
@@ -0,0 +1,13 @@
+package com.auto.autoservice.services.user;
+
+import com.auto.autoservice.model.auth.User;
+
+import java.util.List;
+
+
+public interface UserService {
+
+    List<User> findAll();
+
+
+}
